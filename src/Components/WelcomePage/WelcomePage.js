fad349cf5482468e68091c061f10c648aa157185
Nathaniel Millard <nathanielmillard@gmail.com>
2020-11-07 16:53:17 -0700

    add basic styling to welcome page.

--- src/Components/WelcomePage/WelcomePage.js
+++ src/Components/WelcomePage/WelcomePage.js
@@ -1,10 +1,10 @@
-
+import './WelcomePage.scss'
 import {Link} from "react-router-dom"
 const WelcomePage = (props ) => {
   return(
     <section className='WelcomePage'>
       <h1> Welcome to a Goat's Tale </h1>
-      <p> A story building experince for you and your sleepy kid </p>
+      <p> A story building experience for you and your sleepy kid </p>
       <Link to='./goatGreeting'>
       <h2>Meet The Goat</h2>
       </Link>
