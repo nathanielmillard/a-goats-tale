cc97868a7005e611ba943e39b1b85814c1e33d12
Nathaniel Millard <nathanielmillard@gmail.com>
2020-11-07 13:53:29 -0700

    Set up basic welcome page.

---
+++ src/Components/WelcomePage/WelcomePage.js
@@ -0,0 +1,15 @@
+
+import {Link} from "react-router-dom"
+const WelcomePage = (props ) => {
+  return(
+    <section className='WelcomePage'>
+      <h1> Welcome to a Goat's Tale </h1>
+      <p> A story building experince for you and your sleepy kid </p>
+      <Link to='./goatGreeting'>
+      <h2>Meet The Goat</h2>
+      </Link>
+    </section>
+  )
+}
+
+export default WelcomePage
