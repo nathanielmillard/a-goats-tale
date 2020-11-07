b568bcbf3a0487c30b636a49c82a1ccaa6453b94
Nathaniel Millard <nathanielmillard@gmail.com>
2020-11-07 16:52:47 -0700

    Allow for a user's name to be added to the story.

--- src/Components/StoryPage/StoryPage.js
+++ src/Components/StoryPage/StoryPage.js
@@ -1,18 +1,19 @@
 import {mainStories} from '../../exports/storyArrays.js'
  const StoryPage = (props) => {
-   let testArray= [1,2,3,4,5,6,7,8,9,10]
-   let brokenUpStory = mainStories[0].split('_')
-   let finalStory = brokenUpStory.reduce((finalStory, currentSentence, index) => {
-     if (testArray[index]) {
-       finalStory = finalStory + currentSentence + testArray[index]
-     }
-     else {
-       finalStory = finalStory + currentSentence
-     }
-     return finalStory
-   }, '')
+
+   let brokenUpStory = mainStories[0].split('*')
+   let namedStory = brokenUpStory.join(` ${props.name} `)
+   // let namedStory = brokenUpStory.reduce((finalStory, currentSentence, index) => {
+   //   if (testArray[index]) {
+   //     finalStory = finalStory + currentSentence + testArray[index]
+   //   }
+   //   else {
+   //     finalStory = finalStory + currentSentence
+   //   }
+   //   return finalStory
+   // }, '')
    return (
-    <p>{finalStory}</p>
+    <p>{namedStory}</p>
    )
  }
 
