768c7c9a26cff786a2987de31b45dc74d3b3e415
Nathaniel Millard <nathanielmillard@gmail.com>
2020-11-09 14:46:06 -0700

    Refactor to classed based component.

--- src/Components/StoryPage/StoryPage.js
+++ src/Components/StoryPage/StoryPage.js
@@ -1,32 +1,53 @@
 import './StoryPage.scss'
 import {mainStories} from '../../exports/storyArrays.js'
- const StoryPage = (props) => {
-   let brokenUpStory = mainStories[0].split('*')
-   let namedStory = brokenUpStory.join(` ${props.name} `)
-   brokenUpStory = namedStory.split('_')
-   let finalStory = brokenUpStory.reduce((finalStory, currentSentence, index) => {
-     if (props.words[index]) {
-       finalStory = finalStory + currentSentence + props.words[index]
-     }
-     else {
-       finalStory = finalStory + currentSentence
-     }
-     return finalStory
-   }, '')
-   let sections = finalStory.split('(')
-   sections = sections.map((section, index) => {
-     return <p key={index}>{section}</p>
-   })
+import StoryTile from '../StoryTile/StoryTile.js'
+import {Component} from 'react'
+class StoryPage extends Component {
+  constructor(props) {
+    super(props)
+    this.state= {
+
+    }
+  }
+  createStory () {
+    let brokenUpStory = mainStories[0].split('*')
+    let namedStory = brokenUpStory.join(` ${this.props.name} `)
+    brokenUpStory = namedStory.split('_')
+    let finalStory = brokenUpStory.reduce((finalStory, currentSentence, index) => {
+      if (this.props.words[index]) {
+        finalStory = finalStory + currentSentence + this.props.words[index]
+      }
+      else {
+        finalStory = finalStory + currentSentence
+      }
+      return finalStory
+    }, '')
+    let sections = finalStory.split('(')
+    return sections.map((section, index) => {
+      return <p key={index}>{section}</p>
+    })
+  }
+  createStoryCards() {
+    let storyCards = []
+    let populateCards = () => {
+
+    }
+  }
+ render() {
    return (
-    <section className='StoryPage'>
-      {sections}
-      <h2> The End! </h2>
-      <section className='Button Pannel'>
-        <button onClick={props.saveStory}> Save This Story </button>
-        <button> See Saved Stories </button>
-      </section>
-    </section>
+     <section className='StoryPage'>
+     {this.createStory()}
+     <h2> The End! </h2>
+     <section className='Button Pannel'>
+     <button onClick={this.props.saveStory}> Save This Story </button>
+     <button onClick={this.popularStoryCards}> See Saved Stories </button>
+     </section>
+     <section className='storyTiles'>
+     {this.createStoryTiles}
+     </section>
+     </section>
    )
  }
+}
 
 export default StoryPage
