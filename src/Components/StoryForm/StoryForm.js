3dea78f32b0c7e27247966248feb07097bc1d66c
Nathaniel Millard <nathanielmillard@gmail.com>
2020-11-08 19:56:10 -0700

    Allow for inputs to save neccesary words.

--- src/Components/StoryForm/StoryForm.js
+++ src/Components/StoryForm/StoryForm.js
@@ -5,8 +5,16 @@ class StoryForm extends Component {
   constructor(props){
     super(props)
     this.state = {
-
+      name: '',
+      words: [],
     }
+  }
+  updateWords = (e) => {
+    let id = e.target.id
+    let allWords = this.state.words
+    allWords[id] = e.target.value
+    // console.log(allWords)
+    // this.setState({words: e.target.value})
   }
   saveInfo = () => {
     this.props.saveInputs(this.state.name, this.state.words)
@@ -27,10 +35,40 @@ class StoryForm extends Component {
       </section>
       <section className="wordChoice">
         <label>
-          Name three adjectives:
-          <input type='text'/>
-          <input type='text'/>
-          <input type='text'/>
+          A name for a town:
+          <input id='0' type='text' onChange={this.updateWords}/>
+        </label>
+        <label>
+          A crop
+          <input id='1' type='text' onChange={this.updateWords}/>
+        </label>
+        <label>
+          Two adjectives
+          <input id='2' type='text' onChange={this.updateWords}/>
+          <input id='3' type='text' onChange={this.updateWords}/>
+        </label>
+        <label>
+          One number:
+          <input id='4' type='text' onChange={this.updateWords}/>
+        </label>
+        <label>
+          One name for a person:
+          <input id='5' type='text' onChange={this.updateWords}/>
+        </label>
+        <label>
+          Four adjectives
+          <input id='6' type='text' onChange={this.updateWords}/>
+          <input id='7' type='text' onChange={this.updateWords}/>
+          <input id='8' type='text' onChange={this.updateWords}/>
+          <input id='9' type='text' onChange={this.updateWords}/>
+        </label>
+        <label>
+          A fruit:
+          <input id='10' type='text' onChange={this.updateWords}/>
+        </label>
+        <label>
+          One noun:
+          <input id='11' type='text' onChange={this.updateWords}/>
         </label>
         <Link to='/story' onClick={this.saveInfo}>
         Read Your Story
