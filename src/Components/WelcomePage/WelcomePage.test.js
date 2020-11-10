8bbd5cd1bd6b5e19017088217bb11348d30c5e80
Nathaniel Millard <nathanielmillard@gmail.com>
2020-11-09 18:15:55 -0700

    Write unit tests for WelcomePage.

---
+++ src/Components/WelcomePage/WelcomePage.test.js
@@ -0,0 +1,25 @@
+import WelcomePage from './WelcomePage.js'
+import {render, screen} from '@testing-Library/react'
+import {MemoryRouter} from 'react-router-dom'
+
+describe('Welcome Page', () => {
+  beforeEach(()=>{
+    render (
+      <MemoryRouter>
+        <WelcomePage />
+      </MemoryRouter>
+    )
+  });
+  it('Should render a greeting', () => {
+    let greeting = screen.getByText("Welcome to a Goat's Tale")
+    let description = screen.getByText("A story building experience for you and your sleepy kid")
+    let button = screen.getByText("Meet The Goat")
+    expect(greeting).toBeInTheDocument()
+    expect(description).toBeInTheDocument()
+    expect(button).toBeInTheDocument()
+  });
+  it('Should have a navigation button', () => {
+    let button = screen.getByRole("button")
+    expect(button).toBeInTheDocument()
+  });
+});
