c8e4618606c68e76e620671f6aa7fbdc575c3a20
Nathaniel Millard <nathanielmillard@gmail.com>
2020-11-25 13:49:04 -0700

    Fix testing errors made by text change.

--- src/Components/WelcomePage/WelcomePage.js
+++ src/Components/WelcomePage/WelcomePage.js
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types'
 const WelcomePage = (props ) => {
   return(
     <section className='WelcomePage'>
-      <h1> Welcome to a Goat's Tale ! </h1>
+      <h1> Welcome to a Goat's Tale </h1>
       <p> A story building experience for you and your sleepy kid </p>
       <Link to='./storyForm'>
       <button>Meet The Goat</button>
